da6da868214dbe1c19f1a8d34863c62f4589e41a
Joshua Gray <joshua.gray2@my.jcu.edu.au>
2019-10-01 15:53:30 +1000

    Implement Basic Project Structure

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php
-    require_once("header.php");
-?>
-<div id="container">
-    <div id="content">
-        <!-- content -->
-        <h2> Here is the content of the file </h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
-    <?php
-        require_once("sidebar.php");
-    ?>
-</div>
-<?php
-    require_once("footer.php");
-?>
+    require_once("./resources/config.php");
+    require_once(LIBRARY_PATH . "/templateFunctions.php");
+
+    renderLayoutWithContentFile("home.php");
---
+++ resources/config.php
@@ -0,0 +1,33 @@
+<?php
+/*
+  Configuration file for the Elements Web-Server. This file and its directory
+  should be placed outside the apache base directory when used in production.
+  For ease of use, it is included as a subdirectory to ease development
+*/
+
+$config = array(
+  "db" => array(
+    "dbname" => "elements",
+    "username" => "root",
+    "password" => "root",
+    "host" => "localhost"
+  ),
+  "paths" => array(
+      "resources" => $_SERVER["DOCUMENT_ROOT"] . "/resources",
+      "images" =>array(
+        "content" => $_SERVER["DOCUMENT_ROOT"] . "/img/content",
+        "layout" => $_SERVER["DOCUMENT_ROOT"] . "/img/layout")
+  )
+);
+
+defined("LIBRARY_PATH")
+    or define("LIBRARY_PATH", realpath(dirname(__FILE__) . '/library'));
+
+defined("TEMPLATES_PATH")
+    or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . '/templates'));
+
+/*
+    Error reporting.
+*/
+ini_set("error_reporting", "true");
+error_reporting(E_ALL|E_STRCT);
