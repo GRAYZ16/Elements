2536d077e77eda634b5045246da0a13f6a4ccb24
Joshua Gray <joshua.gray2@my.jcu.edu.au>
2019-10-25 12:35:50 +1000

    Finish Template Layout. Add Files for Navbar pages

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -3,11 +3,12 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="./css/style.css">
     <title>Elements Dashboard</title>
   </head>
   <body>
 
     <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-      <a href="#" class="navbar-brand col-sm-3 col-md-2 mr-0">Elements</a>
+      <a href="#" class="navbar-brand col-sm-3 col-md-2 mr-0"><i class="fa fa-leaf" style="padding-right: 10px;"></i>Elements</a>
     </nav>
--- resources/templates/leftPanel.php
+++ resources/templates/leftPanel.php
@@ -1,9 +1,9 @@
 <nav class="col-md-2 d-none d-md-block bg-light sidebar">
   <div class="sidebar-sticky">
-    <ul class="nav flex-column">
-            <li class="nav-item"><a href="#">Home</a></li>
-            <li class="nav-item"><a href="#">Devices</a></li>
-            <li class="nav-item"><a href="#">Settings</a></li>
+    <ul class="nav flex-column justify-content-center">
+            <li class="nav-item"><a class="nav-link active"href="http://192.168.33.10/Elements/index.php?page=home"><i class="fa fa-home"></i>Home</a></li>
+            <li class="nav-item"><a class="nav-link"href="http://192.168.33.10/Elements/index.php?page=devices"><i class="fa fa-microchip"></i>Devices</a></li>
+            <li class="nav-item"><a class="nav-link"href="http://192.168.33.10/Elements/index.php?page=settings"><i class="fa fa-sliders"></i>Settings</a></li>
     </ul>
   </div>
 </nav>
