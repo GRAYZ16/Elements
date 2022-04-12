99828f2b2cb0cc3cb01757e227ecedb594fe7b40
GRAYZ16 <joshua.gray2@my.jcu.edu.au>
2022-04-12 20:58:08 +1000

    Starter React Added

---
+++ elements-dashboard/src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+    
+    render() {
+
+      return (
+        <div className="App">
+          <h1>Hello, React</h1>
+          <p>Does Auto update work</p>
+        </div>
+      )
+
+    }
+}
+
+export default App;
